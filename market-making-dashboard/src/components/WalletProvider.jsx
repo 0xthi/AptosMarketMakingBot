7eafc86f73f66d734ebb52cca7c0970f6aab7475
Thileepan-Ilankumaran <112769282+Thileepan-Ilankumaran@users.noreply.github.com>
2024-09-27 23:25:36 +0530

    vercel deployment

--- market-making-dashboard/src/components/WalletProvider.jsx
+++ market-making-dashboard/src/components/WalletProvider.jsx
@@ -3,7 +3,7 @@ import {AptosWalletAdapterProvider} from '@aptos-labs/wallet-adapter-react';
 import { Network } from '@aptos-labs/ts-sdk';
 
 const WalletProvider = ({ children }) => {
-  const manifestURL = 'https://assets.mz.xyz/static/config/mizuwallet-connect-manifest.json';
+  const manifestURL = 'https://aptos-market-making-bot.vercel.app/mizuwallet-connect-manifest.json';
 
   return (
     <AptosWalletAdapterProvider
